8e5fa7b522591a869e17dd192cf7345036b78447
JustinMaina <justinmwangi58@gmail.com>
2024-05-26 13:57:10 +0300

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length = 9):
+    '''prints the fibonacci sequence'''
+    if length == 0:
+        print('[]')
+
+    elif length == 1:
+        print('[0]')   
+         
+    else:
+        fib = [0, 1]
+        for i in range(2, length):
+            fib.append(fib[i-1] + fib[i-2])
+        print(fib)
+
+print_fibonacci()        
